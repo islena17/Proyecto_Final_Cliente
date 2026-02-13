e4566bd6d203650c503cf753b981be99fcdd48c4
Islena <islenapolo17@gmail.com>
2026-02-13 08:38:28 +0100

    intento añadir localstorage para las tareas pero hay un bucle

--- Proyecto_final_cliente/src/App.tsx
+++ Proyecto_final_cliente/src/App.tsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Tarea } from "./models/Tarea"
 import FormularioTarea from './components/FormularioTarea'
 import MostrarPendientes from './components/MostarPendientes'
 import MostrarCompletadas from './components/MostrarCompletadas'
 import MostrarAtrasadas from './components/MostrarAtrasadas'
 
+const guardar_tareas = "tareas";
 function App() {
 
   //aqui pongo toda la logica para las tareas
   const [listaTareas, setListaTareas] = useState<Tarea[]>([]);
+
+  //cargamos los datos
+
+
+  useEffect(() => {
+    const tareasStorage = localStorage.getItem(guardar_tareas);
+    if (!tareasStorage) return;
+
+    try {
+      const parsed = JSON.parse(tareasStorage) as any[];
+
+      const tareas = parsed.map((t) => {
+        const tarea = new Tarea(
+          t.nombre,
+          t.titulo,
+          t.descripcion,
+          new Date(t.fechaLimite)
+        );
+
+        tarea.id = t.id;
+        tarea.estado = t.estado ?? (t.completada ? "completada" : "pendiente");
+        tarea.completada = Boolean(t.completada);
+        tarea.fechaCreacion = new Date(t.fechaCreacion);
+
+        return tarea;
+      });
+
+      setListaTareas(tareas);
+    } catch {
+
+    }
+  }, []);
+  
+  useEffect(() => { //este es para guardar tareas
+    localStorage.setItem(guardar_tareas, JSON.stringify(listaTareas));
+  }, [listaTareas]);
 
   //aqui pongo la logica para completar las tareas 
   const completarTarea = (id: string) => {
